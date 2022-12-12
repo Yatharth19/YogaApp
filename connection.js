805c5504bfa4faee12c13c28356e37d2edb9fb7e
Yatharth Agarwal <yatharth.fractal@gmail.com>
2022-12-12 13:13:56 +0530

    initial-commit

---
+++ connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2")
+require('dotenv').config();
+
+const con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    port: process.env.PORT
+});
+
+con.connect((err) => {
+    if(err){
+        throw err;}
+    console.log("Connection created");
+})
+
+// connect()
+module.exports.con = con;
